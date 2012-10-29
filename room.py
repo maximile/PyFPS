6de07c1acccaacfc5c0d64ca548303fb3c116a02
Max Williams <max.williams@framestore.com>
2012-10-29 20:17:12 +0000

    Screenshot

--- room.py
+++ room.py
@@ -251,8 +251,8 @@ class Room(object):
         """Create lightmaps for the floor and ceiling.
 
         """
-        width = 16.0
-        height = 16.0
+        width = 128.0
+        height = 128.0
 
         self.floor_lightmap = Lightmap(width, height)
         self.ceiling_lightmap = Lightmap(width, height)
@@ -265,7 +265,7 @@ class Room(object):
         """Create a lightmap for the wall, and tex coords for it.
         
         """
-        height = 8.0
+        height = 32.0
         
         # Add up the wall lengths to find the width
         total_wall_length = 0.0
